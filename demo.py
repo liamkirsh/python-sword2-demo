e390153e98ea8cf33f1ff8d291309323fab7d254
Liam Kirsh <liam.kirsh@cern.ch>
2016-06-29 17:16:25 +0200

    BasicUsage test

---
+++ demo.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+from sword2 import *
+import logging
+
+SD_URI = "http://localhost:8080/sd-uri"
+user = "sword"
+pw = "sword"
+
+def main():
+    logging.basicConfig() # NSA is watchinh
+    c = Connection(SD_URI, user_name=user, user_pass=pw)
+
+    #import ipdb; ipdb.set_trace()
+
+    c.get_service_document()
+    assert c.sd != None
+    assert c.sd.parsed
+    assert c.sd.valid
+
+    print len(c.workspaces)
+
+    workspace_1_title, workspace_1_collections = c.workspaces[0]
+    collection = workspace_1_collections[0]
+
+    #print collection # get a human-readable report for it
+
+    with open("foo.jpg", "rb") as data:
+        # docs are outdated: create replaced create_resource
+        receipt = c.create(col_iri = collection.href,
+                                    payload = data,
+                                    mimetype = "application/zip",
+                                    filename = "foo.jpg",
+                                    packaging = "http://purl.org/net/sword/package/Binary")
+    #print "\n\n\n" + str(receipt)
+
+    e = Entry(id = "atomid",
+                title = "atom title")
+    #receipt2 = c.create(col_iri = collection.href,
+    #                    metadata_entry = e)
+    e.add_fields(dcterms_title = "dcterms title", dcterms_some_other_field = "other") 
+    e.add_fields(author={"name":"Ben", "email":"foo@example.org"})
+    print str(e)
+    e.register_namespace("myschema", "http://example.org")
+    e.add_fields(myscema_foo = "bar")
+    receipt2 = c.create(col_iri = collection.href, metadata_entry = e)
+
+if __name__ == '__main__':
+    main()
